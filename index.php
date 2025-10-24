f2818774d33d8b5e211a21ed377a65c47413d2bd
pedro-naider <pedronaider@gmail.com>
2025-10-24 20:04:22 -0300

    Começando projeto de PHP

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// definir constante para controlar o fluxo da aplicação
+define('CONTROL', true);
+
+// incluir arquivos
+$routes = require_once('inc/routes.php');
+
+// definir rota
+$route = $_GET['route']; ??
